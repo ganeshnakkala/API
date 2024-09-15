cd6875e2a57c853bcdef53b842c4074c8385c002
Ushasree Kamala <ushasree210@gmail.com>
2024-09-15 17:06:31 -0500

    Added source code for web APIs with Swagger

---
+++ Greetings/GreetingAPI/Controllers/HelloAPIController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GreetingAPI.Controllers
+{
+ 
+        [Route("api/HelloAPI")]
+        [ApiController]
+        public class HelloAPIController : ControllerBase
+        {
+            [HttpGet]
+            public ActionResult<String> Hello()
+            {
+                return Ok("Hello");
+            }
+            [HttpGet("name")]
+            public ActionResult<String> HelloName(string name)
+            {
+                name = name.Trim();
+                if (name == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+
+                    foreach (char c in name)
+                    {
+                        if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
+                        {
+                            return BadRequest("Name cannot be digits or special characters");
+                        }
+                    }
+                    return Ok("Hello " + name);
+
+
+                }
+            }
+        }
+    }
